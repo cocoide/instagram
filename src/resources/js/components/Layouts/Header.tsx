0326a89d9c7d87373511f292cef5d0bd1caf129a
{cocoide} <{cocoide.com@gmail.com}>
2023-04-07 19:34:22 +0900

    🔐 update login and handleAuth logic

--- src/resources/js/components/Layouts/Header.tsx
+++ src/resources/js/components/Layouts/Header.tsx
@@ -4,16 +4,22 @@ import useAuth from '../../hooks/useAuth'
 import { loginSelector } from '../../stores/recoil'
 import { useRecoilValue } from 'recoil'
 
-const header_body = 'bg-white h-15 border-b border-gray-200 flex flex-row justify-center p-3 space-x-10 text-gray-400'
+const header_body = 'p-2 relative bg-white h-13 border-b border-gray-200 flex flex-row justify-center items-center space-x-10 text-gray-400'
+const image_loader = "h-10 w-10 bg-gray-200 rounded-full animate-pulse"
 
 const Header = () => {
-  const { isLogin } = useRecoilValue(loginSelector)
+  const { isLogin, img_src, name } = useRecoilValue(loginSelector)
   const { logout } = useAuth()
   return (
     <div className={clsx(header_body)}>
       <Link to="/home">ホーム</Link>
       {isLogin ? <button onClick={() => logout()}>ログアウト</button> : <Link to="/login">ログイン</Link>}
       <Link to="/post">投稿</Link>
+      {img_src ?
+        <img src={img_src} alt={name} className="h-10 w-10 rounded-full  ring-1 ring-gray-300 shadow-sm" />
+        :
+        <div className={clsx(image_loader)} />
+      }
     </div>
   )
 }
